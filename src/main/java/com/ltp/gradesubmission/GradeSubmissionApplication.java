d212f135244d53e118af0d9e8e8d028743b199af
Sri Ganesh <manu@Sris-MacBook-Air.local>
2023-04-25 15:08:16 +0530

    adding names and courses

--- src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -29,10 +29,10 @@ public class GradeSubmissionApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Student[] students = new Student[] {
-            new Student("Harry Potter", LocalDate.parse(("1980-07-31"))),
-            new Student("Ron Weasley", LocalDate.parse(("1980-03-01"))),
-            new Student("Hermione Granger", LocalDate.parse(("1979-09-19"))),
-            new Student("Neville Longbottom", LocalDate.parse(("1980-07-30"))) 
+            new Student("Shankar ", LocalDate.parse(("1980-07-31"))),
+            new Student("Arnav Patil", LocalDate.parse(("1980-03-01"))),
+            new Student("Amritpal", LocalDate.parse(("1979-09-19"))),
+            new Student("Krishna Gupta", LocalDate.parse(("1980-07-30"))) 
         };
 		
 		for (int i = 0; i < students.length; i++) {
@@ -40,12 +40,11 @@ public class GradeSubmissionApplication implements CommandLineRunner {
 		}
 
 		Course[] courses = new Course[] {
-            new Course("Charms", "CH104", "In this class, you will learn spells concerned with giving an object new and unexpected properties."),
-            new Course("Defence Against the Dark Arts", "DADA", "In this class, you will learn defensive techniques against the dark arts."),
-            new Course("Herbology", "HB311", "In this class, you will learn the study of magical plants and how to take care of, utilise and combat them."),
-            new Course("History of Magic", "HIS393", "In this class, you will learn about significant events in wizarding history."),
-            new Course("Potions", "POT102", "In this class, you will learn correct mixing and stirring of ingredients to create mixtures with magical effects."),
-            new Course("Transfiguration", "TR442", "In this class, you will learn the art of changing the form or appearance of an object.")
+            new Course("Biology", "CH104", "In this class, you will learn basic principles of plant and animal life."),
+            new Course("Maths", "DADA", "In this class higher level algrbra and geometry."),
+            new Course("Herbology", "HB311", "In this class, you will learn the study of medicinal plants and how to take care of, utilise and combat them."),
+            new Course("History", "HIS393", "In this class, you will learn about significant events in world history."),
+            new Course("Chemistry lab", "POT102", "In this class, you will learn correct mixing and stirring of ingredients to create mixtures with effects."),
         };
 
 		for (int i = 0; i < courses.length; i++) {
